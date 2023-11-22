4533772ee9e0b9271d341e70b540ef41c7c052a5
Davemafy <imafidondavid1@gmail.com>
2023-11-22 14:24:48 +0100

    added color

--- build/tailwind.config.js
+++ build/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["../*.html"],
   theme: {
     extend: {
       colors: {
-        
+        veryDarkBlue: "hsl(233, 47%, 7%)",
+        darkDesaturatedBlue: 'hsl(244, 38%, 16%)',
+        softViolet: 'hsl(277, 64%, 61%)',
+        white: 'hsl(0, 0%, 100%)',
+        transWhite: 'hsla(0, 0%, 100%, 0.75)',
+        transWhite2: 'hsla(0, 0%, 100%, 0.6)',        
       },
     },
   },
